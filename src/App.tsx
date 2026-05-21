4700414713ab600846798e6d012a38ecc542b05b
Malte <91071750+Loxiuz@users.noreply.github.com>
2026-05-21 17:46:08 +0200

    refactor routing structure and improve user page image handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ExportPage from "./export/ExportPage";
 import Layout from "./layout/Layout";
@@ -16,40 +11,60 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route path="/" element={
-          <RequireAuth allowedRoles={["ROLE_USER", "ROLE_ADMIN"]}>
-            <Layout />
-          </RequireAuth>
-        }>
-          <Route path="export" element={
-            <RequireAuth allowedRoles={["ROLE_ADMIN"]}>
-              <ExportPage />
+        <Route
+          path="/"
+          element={
+            <RequireAuth allowedRoles={["ROLE_USER", "ROLE_ADMIN"]}>
+              <Layout />
             </RequireAuth>
-          } />
-          <Route path="import" element={
-            <RequireAuth allowedRoles={["ROLE_ADMIN"]}>
-              <h1>Import</h1>
-            </RequireAuth>
-          } />
-          <Route path="flight-schedules" element={
-            <RequireAuth allowedRoles={["ROLE_ADMIN"]}>
-              <h1>Flight Schedules</h1>
-            </RequireAuth>
-          } />
-          <Route path="cargo-tracking" element={
-            <RequireAuth allowedRoles={["ROLE_ADMIN"]}>
-              <h1>Cargo Tracking</h1>
-            </RequireAuth>
-          } />
-          <Route path="warehouse" element={
-            <RequireAuth allowedRoles={["ROLE_ADMIN"]}>
-              <h1>Warehouse</h1>
-            </RequireAuth>
-          } />
+          }
+        >
+          <Route
+            path="export"
+            element={
+              <RequireAuth allowedRoles={["ROLE_ADMIN"]}>
+                <ExportPage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="import"
+            element={
+              <RequireAuth allowedRoles={["ROLE_ADMIN"]}>
+                <h1>Import</h1>
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="flight-schedules"
+            element={
+              <RequireAuth allowedRoles={["ROLE_ADMIN"]}>
+                <h1>Flight Schedules</h1>
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="cargo-tracking"
+            element={
+              <RequireAuth allowedRoles={["ROLE_ADMIN"]}>
+                <h1>Cargo Tracking</h1>
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="warehouse"
+            element={
+              <RequireAuth allowedRoles={["ROLE_ADMIN"]}>
+                <h1>Warehouse</h1>
+              </RequireAuth>
+            }
+          />
+          {/* TODO: wrap with RequireAuth */}
           <Route path="user" element={<UserPage />} />
           <Route path="settings" element={<h1>Settings</h1>} />
           <Route path="unauthorized" element={<h1>Unauthorized</h1>} />
           <Route path="*" element={<Navigate to="export" />} />
+          {/* TODO: add group page for users to communicate with threads and comments */}
         </Route>
       </Routes>
     </BrowserRouter>
--- src/user/UserPage.tsx
+++ src/user/UserPage.tsx
@@ -1,90 +1,92 @@
 import React, { useEffect, useState } from "react";
-import "./UserPage.css"
+import "./UserPage.css";
 import { getEmployeeImage, uploadImageToEmployee } from "../api/employeeApi";
 export default function UserPage() {
+  const [formData, setFormData] = useState({
+    image: null as File | null,
+    employeeId: localStorage.getItem("employeeId") || "",
+    username: localStorage.getItem("username") || "",
+    role: localStorage.getItem("role") || "",
+  });
 
-    const [formData, setFormData] = useState({
-        image: null as File | null,
-        employeeId: localStorage.getItem("employeeId") || "",
-        username: localStorage.getItem("username") || "",
-        role: localStorage.getItem("role") || "",
-    });
+  useEffect(() => {
+    const fetchEmployeeImage = async () => {
+      try {
+        const imageBlob = await getEmployeeImage(formData?.employeeId);
+        const imageUrl = URL.createObjectURL(imageBlob);
+        const imgElement = document.querySelector(
+          "#img-container img",
+        ) as HTMLImageElement;
+        imgElement.src = imageUrl;
+        imgElement.alt = "Employee Image";
+      } catch (error) {
+        console.error("Error fetching employee image:", error);
+      }
+    };
+    fetchEmployeeImage();
+  }, [formData.employeeId]);
 
-    useEffect(() => {
-        const fetchEmployeeImage = async () => {
-            try {
-                const imageBlob = await getEmployeeImage(formData?.employeeId);
-                const imageUrl = URL.createObjectURL(imageBlob);
-                const imgElement = document.querySelector(
-                    "#img-container img"
-                ) as HTMLImageElement;
-                imgElement.src = imageUrl;
-                imgElement.alt = "Employee Image";
-            } catch (error) {
-                console.error("Error fetching employee image:", error);
-            }
+  function handleFormChange(e: React.ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (e.target.type === "file") {
+      setFormData((prev) => ({
+        ...prev,
+        image: e.target.files ? e.target.files[0] : null,
+      }));
+    } else {
+      setFormData((prev) => ({
+        ...prev,
+        [e.target.name]: e.target.value,
+      }));
+    }
+  }
 
-        }
-        fetchEmployeeImage();
-    }, [formData.employeeId]);
+  async function handleFormSubit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-
-    function handleFormChange(e: React.ChangeEvent<HTMLFormElement>) {
-        e.preventDefault();
-        if (e.target.type === "file") {
-            setFormData(prev => ({
-                ...prev,
-                image: e.target.files ? e.target.files[0] : null,
-            }));
-        } else {
-            setFormData(prev => ({
-                ...prev,
-                [e.target.name]: e.target.value,
-            }));
-        }
+    const { image, employeeId } = formData;
+    if (!image) {
+      alert("Please select an image to upload.");
+      return;
     }
 
-    async function handleFormSubit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
+    await uploadImageToEmployee(employeeId, image);
+    globalThis.location.reload();
+  }
 
-        const { image, employeeId } = formData;
-        if (!image) {
-            alert("Please select an image to upload.");
-            return;
-        }
-
-        await uploadImageToEmployee(employeeId, image);
-        globalThis.location.reload();
-    }
-
-
-    return (
-        <div id="user-page-container">
-            <h1>User Page</h1>
-            <div id="user-info-grid">
-                <div id="img-container" className="user-info-item">
-                    <img src="" alt="" />
-                    <form id="edit-img-form" onSubmit={handleFormSubit} onChange={handleFormChange}>
-                        <input type="file" id="edit-img-input" name="image" />
-                        <input type="submit" id="edit-img-btn" value="Upload" />
-                    </form>
-                </div>
-                <div id="employee-id-container" className="user-info-item">
-                    <p>Employee ID: {formData.employeeId && (
-                        <span>{formData.employeeId}</span>
-                    )}</p>
-                </div>
-                <div id="username-container" className="user-info-item">
-                    <p>Username: {formData.username && (
-                        <span>{formData.username}</span>
-                    )}</p>
-                </div>
-                <div id="role-container" className="user-info-item">
-                    <p>Role: {formData.role && (
-                        <span>{formData.role.split("_")[1]}</span>
-                    )}</p>
-                </div>
-            </div>
+  return (
+    <div id="user-page-container">
+      <h1>User Page</h1>
+      <div id="user-info-grid">
+        <div id="img-container" className="user-info-item">
+          <img src="" alt="" />
+          <form
+            id="edit-img-form"
+            onSubmit={handleFormSubit}
+            onChange={handleFormChange}
+          >
+            <input type="file" id="edit-img-input" name="image" />
+            <input type="submit" id="edit-img-btn" value="Upload" />
+          </form>
         </div>
-    );
+        <div id="employee-id-container" className="user-info-item">
+          <p>
+            Employee ID:{" "}
+            {formData.employeeId && <span>{formData.employeeId}</span>}
+          </p>
+        </div>
+        {/* TODD: Add full name with element with option to hide for other users but not for admin. Or change visibility on a settings page */}
+        <div id="username-container" className="user-info-item">
+          <p>
+            Username: {formData.username && <span>{formData.username}</span>}
+          </p>
+        </div>
+        <div id="role-container" className="user-info-item">
+          <p>
+            Role: {formData.role && <span>{formData.role.split("_")[1]}</span>}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
